14d83f9adce87b4be0436f791c413eea9fce22dc
TashaC1126 <tashacan84@gmail.com>
2026-01-22 02:06:51 +0000

    added 1 UT file for the customer-service

--- infrastructure/msp-9-jenkins-server-template/jenkinsdata.sh
+++ infrastructure/msp-9-jenkins-server-template/jenkinsdata.sh
@@ -5,14 +5,14 @@ dnf update -y
 hostnamectl set-hostname jenkins-server
 # install git
 dnf install git -y
-# install java 11
-dnf install fontconfig java-11-amazon-corretto -y
+# install java 17
+dnf install fontconfig java-17-amazon-corretto-devel -y
 
 # install jenkins
 wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-rpm --import https://pkg.jenkins.io/redhat/jenkins.io-2023.key
+rpm --import  https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 dnf upgrade -y
-dnf install jenkins-2.452.3-1.1 -y
+dnf install jenkins -y
 
 # install docker
 dnf install docker -y
@@ -20,7 +20,7 @@ systemctl start docker
 systemctl enable docker
 usermod -a -G docker ec2-user
 usermod -a -G docker jenkins
-systemctl enable jenkins
+
 systemctl start jenkins
 # install docker compose
 curl -SL https://github.com/docker/compose/releases/download/v2.36.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
